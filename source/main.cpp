d9e2f416bc5e12477b3364f4d6aa82621a1853b7
vstanislao29 <vstanislao29@my.whitworth.edu>
2025-11-11 17:30:16 -0800

    added images and movement. got graphics working

--- source/main.cpp
+++ source/main.cpp
@@ -1,130 +1,138 @@
 #include <windows.h>
 #include <gdiplus.h>
+#include <iostream>
+using namespace std;
 using namespace Gdiplus;
-#define byte Gdiplus::Byte
 
-#pragma comment(lib, "gdiplus.lib")
+#pragma comment(lib, "Gdiplus.lib")
 
-// Global variables
+// Player position
+int playerX = 100;
+int playerY = 100;
+
+// Track keys being pressed
+bool keys[256] = { false };
+
+// Flag to run the game loop
+bool running = true;
+
+// Global GDI+ objects
 ULONG_PTR gdiplusToken;
-int x = 400, y = 300;  // Circle position
-bool keyW = false, keyA = false, keyS = false, keyD = false;
-const int speed = 5;
-const int WIN_WIDTH = 800, WIN_HEIGHT = 600;
+GdiplusStartupInput gdiplusStartupInput;
+Image* shipIdle = nullptr;
 
-// Forward declaration
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+// Forward declare draw function
+void Draw(HDC hdc, int width, int height);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
-{
+// Update movement
+void update() {
+    if(playerX > 0){
+        if (keys['A']) playerX -= 5;
+    }
+    if(playerX < 700){
+        if (keys['D']) playerX += 5;
+    }
+    if(playerY > 0){
+        if (keys['W']) playerY -= 5;
+    }
+    if(playerY < 520){
+        if (keys['S']) playerY += 5;
+    }
+
+    cout << "X: " << playerX << " Y: " << playerY << endl;
+}
+
+// Game window procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_KEYDOWN:
+        keys[wParam] = true;
+        break;
+    case WM_KEYUP:
+        keys[wParam] = false;
+        break;
+    case WM_DESTROY:
+        running = false;
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+// Main entry point
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
     // Initialize GDI+
-    GdiplusStartupInput gdiplusStartupInput;
     GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
+    shipIdle = new Image(L"Images/shipIdle.png"); // Load your image here
 
     // Register window class
-    WNDCLASS wc = {};
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX) };
+    wc.style = CS_HREDRAW | CS_VREDRAW;
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = TEXT("GDIPlusWindow");
-    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    RegisterClass(&wc);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wc.lpszClassName = TEXT("MyGameWindow");
+    RegisterClassEx(&wc);
 
     // Create window
     HWND hwnd = CreateWindowEx(
-        0, TEXT("GDIPlusWindow"), TEXT("GDI+ Moving Circle (WASD)"),
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, WIN_WIDTH, WIN_HEIGHT,
-        nullptr, nullptr, hInstance, nullptr
+        0, TEXT("MyGameWindow"), TEXT("My 2D Game"),
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        NULL, NULL, hInstance, NULL
     );
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
-    // Game loop
-    MSG msg = {};
-    while (true)
-    {
-        // Handle messages
-        while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
-        {
-            if (msg.message == WM_QUIT)
-                goto exit;
+    // Main game loop (~60 FPS)
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+
+    MSG msg;
+    while (running) {
+        // Handle Windows messages
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
 
-        // Update movement
-        if (keyW) y -= speed;
-        if (keyS) y += speed;
-        if (keyA) x -= speed;
-        if (keyD) x += speed;
+        update();
 
-        // Keep circle in window bounds
-        if (x < 0) x = 0;
-        if (y < 0) y = 0;
-        if (x > WIN_WIDTH - 50) x = WIN_WIDTH - 50;
-        if (y > WIN_HEIGHT - 50) y = WIN_HEIGHT - 50;
+        // Redraw window
+        HDC hdc = GetDC(hwnd);
+        RECT rect;
+        GetClientRect(hwnd, &rect);
+        int width = rect.right - rect.left;
+        int height = rect.bottom - rect.top;
+        Draw(hdc, width, height);
+        ReleaseDC(hwnd, hdc);
 
-        // Redraw
-        InvalidateRect(hwnd, nullptr, FALSE);
-        Sleep(10); // ~60 FPS
+        Sleep(frameDelay);
     }
 
-exit:
+    // Cleanup
+    delete shipIdle;
     GdiplusShutdown(gdiplusToken);
     return 0;
 }
 
-// Window procedure
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch (msg)
-    {
-    case WM_KEYDOWN:
-        if (wParam == 'W') keyW = true;
-        if (wParam == 'A') keyA = true;
-        if (wParam == 'S') keyS = true;
-        if (wParam == 'D') keyD = true;
-        break;
+// Draw everything here
+void Draw(HDC hdc, int width, int height) {
+    Graphics graphics(hdc);
+    Bitmap buffer(width, height, &graphics);  // Offscreen buffer
+    Graphics g(&buffer);
 
-    case WM_KEYUP:
-        if (wParam == 'W') keyW = false;
-        if (wParam == 'A') keyA = false;
-        if (wParam == 'S') keyS = false;
-        if (wParam == 'D') keyD = false;
-        break;
+    // Fill background black
+    g.Clear(Color(255, 0, 0, 0));
 
-    case WM_PAINT: {
-    PAINTSTRUCT ps;
-    HDC hdc = BeginPaint(hwnd, &ps);
+    // Draw the player image
+    if (shipIdle)
+        g.DrawImage(shipIdle, playerX, playerY, 84, 48);
 
-    // Create a GDI+ Graphics object from the window
-    Gdiplus::Graphics graphics(hdc);
-
-    // --- DOUBLE BUFFER START ---
-    Gdiplus::Bitmap buffer(800, 600, &graphics);   // off-screen buffer
-    Gdiplus::Graphics g(&buffer);                  // draw on buffer
-
-    // Clear background
-    g.Clear(Gdiplus::Color(255, 0, 0, 0)); // white
-
-    // Draw your blue circle
-    Gdiplus::SolidBrush brush(Gdiplus::Color(255, 0, 0, 255));
-    g.FillEllipse(&brush, x, y, 50, 50);
-
-    // Copy buffer to screen in one go
-    graphics.DrawImage(&buffer, 0, 0);
-    // --- DOUBLE BUFFER END ---
-
-    EndPaint(hwnd, &ps);
-    break;
-    }
-    return 0;
-
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    }
-
-    return DefWindowProc(hwnd, msg, wParam, lParam);
+    // Draw buffer to the screen
+    graphics.DrawImage(&buffer, 0, 0, width, height);
 }
