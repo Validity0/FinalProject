2358869a21f7d315f41f83cd0ec2413677a77547
Henry Bonikowsky <96221315+Henry-Bonikowsky@users.noreply.github.com>
2025-11-11 20:08:07 -0800

    Neural Network 1.0

--- source/main.cpp
+++ source/main.cpp
@@ -1,71 +1,28 @@
-#include <SFML/Graphics.hpp>
+#include "NeuralNetwork.h"
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
-void printMap(const vector<vector<int>>& map){
-    int size = map.size();
-    for(int r=0; r<size; r++){
-        for(int c=0; c<size; c++){
-            cout << map[r][c] << " ";
-        }
-        cout << endl;
+int main() {
+    NeuralNetwork nn({3, 4, 4, 1});
+
+    // One training example
+    Matrix input(1, 3);
+    input.data[0] = {0.9f, 0.3f, -0.7f}; // spaceship environment
+
+    Matrix target(1, 1);
+    target.data[0] = {1.0f}; // correct answer: thrust left
+
+    float learningRate = 0.2f;
+
+    // Training loop
+    for (int i = 0; i < 10; ++i) {
+        nn.train(input, target, learningRate);
+
+        Matrix prediction = nn.predict(input);
+        std::cout << "Step " << i + 1 << " prediction: ";
+        prediction.print();
     }
-}
-
-int main(){
-
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Tilemap Example");
-    window.setFramerateLimit(60);
-
-    //0 = empty space
-    int empty = 0;
-
-    //1 = wall
-    int wall = 1;
-
-    //2 = target
-    int target = 2;
-
-    //3 = player
-    int player = 3;
-
-    //mark current tile as visited
-    //select up down left and right of the current
-    //if it is a wall out of range or a wall return failure
-    //if the tile has been visited return failure.
-    //if it is target return success
-    //check the distance from the target for all the values (f,g and h values) and select the closest
-    //f = distance to player
-    //g = distance to target
-    //h = f + g
-    //call the funcion again from the closer tile
-
-    //functions needed
-    //get distance function
-    //get index of tile above below left and right, maybe dont need a function.
-    //randomize walls, target, player?
-    //place walls, than target, than player, if the player index is equal to the target, try again.
-    //remember the random seeds of the indecies of player and target.
-    //has been visited? remember in a hashset probobly.
-
-    int size = 5;
-    vector<vector<int>> map(size, vector<int>(size, 0));
-    /*
-      0  1  2  3
-    [[0, 0, 0, 0],  0
-     [0, 0, 0, 0],  1
-     [0, 1, 0, 0],  2
-     [0, 0, 0, 0]]  3
-    */
-
-    //map[y][x]
-    //set value
-    map[2][1] = 1;
-
-    printMap(map);
-
-
 
     return 0;
 }
